03de4eb9eb84bdb81291cf285a5e15df3ca98884
hindsight <hindsight@b1562d05-e91d-4c1c-b48f-3c04b2a93670>
2013-11-19 19:34:28 +0000

    Try/catch block now surrounds RunUnittests() call in main(...)

--- trunk/femu/src/main.cpp
+++ trunk/femu/src/main.cpp
@@ -1,11 +1,16 @@
 #include <QtCore>
+#include <util/exception.h>
 #include <ut/tests.h>
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
 
     // Run unit tests
-    RunUnittests();
+    try {
+        RunUnittests();
+    } catch (const TException &e) {
+        e.Display();
+    }
 
     return 0;
 }
