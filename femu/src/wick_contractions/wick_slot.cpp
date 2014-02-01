e3d9d5a206779c7ac9f12d49dc2009f156ad246e
sklins <sonnik_007@mail.ru>
2014-02-01 15:59:14 +0000

    contract, break contract

--- femu/src/wick_contractions/wick_slot.cpp
+++ femu/src/wick_contractions/wick_slot.cpp
@@ -15,6 +15,21 @@ bool TWickSlot::EquivalentSlots(const TWickSlot &a, const TWickSlot &b)
 
 bool TWickSlot::AllowConnection(const TWickSlot &a, const TWickSlot &b, uint32_t particleType)
 {
-    ASSERT(particleType >= 0 && particleType < a.CurrentFreedomDegrees.size() && particleType < b.CurrentFreedomDegrees.size());
+    ASSERT(particleType >= 0 && particleType < (uint32_t)a.CurrentFreedomDegrees.size() && particleType < (uint32_t)b.CurrentFreedomDegrees.size());
     return a.CurrentFreedomDegrees[particleType] > 0 && b.CurrentFreedomDegrees[particleType] > 0 && (a.InconsistencyMask & b.InconsistencyMask) != 0;
+}
+
+void TWickSlot::Contract(TWickSlot &a, TWickSlot &b, uint32_t particleType)
+{
+    ASSERT(a.CurrentFreedomDegrees[particleType] > 0 && b.CurrentFreedomDegrees[particleType] > 0);
+    a.CurrentFreedomDegrees[particleType]--;
+    b.CurrentFreedomDegrees[particleType]--;
+}
+
+void TWickSlot::BreakContraction(TWickSlot &a, TWickSlot &b, uint32_t particleType)
+{
+    ASSERT(a.CurrentFreedomDegrees[particleType] < a.InitialFreedomDegrees[particleType]
+        && b.CurrentFreedomDegrees[particleType] < b.InitialFreedomDegrees[particleType]);
+    a.CurrentFreedomDegrees[particleType]++;
+    b.CurrentFreedomDegrees[particleType]++; 
 }
