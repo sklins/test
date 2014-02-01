8172060bf0ef3fa7fee9a9059a682f05665ddec0
sklins <sonnik_007@mail.ru>
2014-02-01 15:15:54 +0000

    equivalent slots, allow connection

--- femu/src/wick_contractions/wick_slot.cpp
+++ femu/src/wick_contractions/wick_slot.cpp
@@ -7,3 +7,14 @@ TWickSlot::TWickSlot(uint32_t particleTypeCount, uint32_t equivalenceClass, uint
     CurrentFreedomDegrees.resize(particleTypeCount);
 }
 
+	
+bool TWickSlot::EquivalentSlots(const TWickSlot &a, const TWickSlot &b)
+{
+    return a.EquivalenceClass == b.EquivalenceClass && a.CurrentFreedomDegrees == b.CurrentFreedomDegrees;
+}
+
+bool TWickSlot::AllowConnection(const TWickSlot &a, const TWickSlot &b, uint32_t particleType)
+{
+    ASSERT(particleType >= 0 && particleType < a.CurrentFreedomDegrees.size() && particleType < b.CurrentFreedomDegrees.size());
+    return a.CurrentFreedomDegrees[particleType] > 0 && b.CurrentFreedomDegrees[particleType] > 0 && (a.InconsistencyMask & b.InconsistencyMask) != 0;
+}
