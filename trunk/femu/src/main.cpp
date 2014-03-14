69191b49110fdce460d35ff660a89d458ed9f017
hindsight <hindsight@b1562d05-e91d-4c1c-b48f-3c04b2a93670>
2014-03-14 11:28:39 +0000

    Multitask dialog in main.cpp

--- trunk/femu/src/main.cpp
+++ trunk/femu/src/main.cpp
@@ -4,12 +4,25 @@
 
 int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
+    QStringList args = app.arguments();
 
-    // Run unit tests
-    try {
-        RunUnittests();
-    } catch (const TException &e) {
-        e.Display();
+    if (args.size() == 1 || args[1] == "ut") {
+        // Run unit tests
+        try {
+            RunUnittests();
+        } catch (const TException &e) {
+            e.Display();
+        }
+    } else {
+        if (args[1] != "help") {
+            MESSAGE("Unknown action: " << args[1]);
+            MESSAGE("");
+        }
+        
+        MESSAGE("Available actions:");
+        
+        MESSAGE("bin/femu ut (or just bin/femu) -- run unittests");
+        MESSAGE("bin/femu help                  -- show this info");
     }
 
     return 0;
