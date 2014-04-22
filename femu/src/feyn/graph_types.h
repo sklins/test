6271185a7b9ab4c1a7098f483e799e4e57420bb5
sklins <sonnik_007@mail.ru>
2014-04-22 13:47:50 +0000

    particle

--- femu/src/feyn/graph_types.h
+++ femu/src/feyn/graph_types.h
@@ -1,3 +1,4 @@
 #pragma once
 class TVertex;
 class TEdge;
+class TParticle;
---
+++ femu/src/feyn/particle.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <util/global.h>
+
+enum ELineShape {
+    LS_NORMAL, LS_DASHED, LS_WAVE, LS_DOTTED
+};
+
+class TParticle
+{
+public:
+    TParticle(QString name, ELineShape lineShape) : Name(name), LineShape(lineShape) {}
+
+public:
+    ELineShape GetLineShape() {return this->LineShape;}
+    
+private:
+    QString Name;
+    ELineShape LineShape;
+};
