4b811a051cb5883c80c7efacd288affa9eac705b
sklins <sonnik_007@mail.ru>
2014-05-09 15:10:30 +0000

    GUI: first step

---
+++ femu/src/gui/step_1.cpp
@@ -0,0 +1,67 @@
+#include "step_1.h"
+
+UI_Step1::UI_Step1(QWidget *parent): QWidget(parent) {
+    ui = new Ui::UI_Step1();
+    ui->setupUi(this);
+
+    move(QApplication::desktop()->screen()->rect().center() - rect().center());
+
+    int n = 3, m = 2;
+
+    svgWidgets.resize(n * m);
+    for (int i = 0; i < n * m; i++) {
+        svgWidgets[i] = new QSvgWidget(this);
+        ui->interactionVertexes->addWidget(svgWidgets[i], i / m, i % m);
+    }
+
+    ImportTheories();
+    ShowTheories();
+
+    QObject::connect(ui->theories, SIGNAL(itemClicked(QListWidgetItem*)), this, SLOT(HandleClick(QListWidgetItem*)));
+    QObject::connect(ui->quitButton, SIGNAL(clicked()), this, SLOT(close()));
+    QObject::connect(ui->nextButton, SIGNAL(clicked()), this, SLOT(Next()));
+}
+
+UI_Step1::~UI_Step1() {
+    delete ui;
+}
+
+void UI_Step1::ImportTheories() {
+    QDir theoriesDir("theories");
+    QStringList xmls = theoriesDir.entryList(QStringList() << "*.xml");
+    for (QStringList::ConstIterator i = xmls.constBegin(); i != xmls.constEnd(); i++) {
+        TFeynRules *rules = new TFeynRules();
+        rules->ImportFromXml(theoriesDir.absoluteFilePath(*i));
+        theories.insert(rules->Name, rules);
+    }
+}
+
+void UI_Step1::ShowTheories() {
+    ui->theories->clear();
+    for (QHash<QString, TFeynRules*>::ConstIterator i = theories.begin();
+         i != theories.end(); i++) {
+        ui->theories->addItem(i.key());
+    }
+}
+
+void UI_Step1::HandleClick(QListWidgetItem *item) {
+    QString theoryName = item->text();
+    TFeynRules *rules = theories.value(theoryName);
+    QVector<QByteArray> svgData;
+    for (QSet<TInteraction*>::ConstIterator i = rules->Interactions.begin();
+         i != rules->Interactions.end(); i++) {
+        TDiagram d;
+        (*i)->GenerateExampleDiagram(&d);
+        svgData << d.GenerateImageData((*i)->Name);
+    }
+
+    for (int i = 0; i < svgWidgets.size(); i++) {
+        if (i < svgData.size()) {
+            svgWidgets[i]->load(svgData[i]);
+        } else
+            svgWidgets[i]->load(QByteArray());
+    }
+}
+
+void UI_Step1::Next() {
+}
--- femu/src/main.cpp
+++ femu/src/main.cpp
@@ -1,81 +1,12 @@
-#include <QtCore>
-#include <util/exception.h>
-#include <ut/tests.h>
-#include <ut/integration/wick_example.h>
-#include <ut/integration/solve_1.h>
-#include <ut/integration/solve_2.h>
-#include <ut/integration/solve_3.h>
-
-#include <feyn/diagram.h>
-#include <feyn/feyn_rules.h>
-void PreGui() {
-    QDir examples("examples");
-    QStringList entries = examples.entryList(QStringList() << "*.xml");
-    int n = entries.size();
-    QVector<TFeynRules> theories; theories.resize(n);
-
-    QTextStream input(stdin), output(stdout);
-
-    for (int i = 0; i < n; i++) {
-        output << "Parsing " << entries[i] << "..\n";
-        output.flush();
-        theories[i].ImportFromXml(examples.absoluteFilePath(entries[i]));
-        output << "Imported #" << i << " " << theories[i].Name << "\n";
-        output.flush();
-    }
-
-    output << "Enter #"; output.flush();
-    int k; input >> k;
-    output << "Generating for " << theories[k].Name << "\n";
-
-    for (QSet<TInteraction*>::ConstIterator i = theories[k].Interactions.constBegin();
-         i != theories[k].Interactions.constEnd(); i++) {
-        TInteraction *in = *i;
-        TDiagram d;
-        in->GenerateExampleDiagram(&d);
-        d.GenerateImage("example", in->Name + ".svg");
-    }
-
-    output.flush();
-}
+#include <util/global.h>
+#include <QtGui>
+#include <gui/step_1.h>
 
 int main(int argc, char *argv[]) {
-    QCoreApplication app(argc, argv);
-    QStringList args = app.arguments();
+    QApplication app(argc, argv);
 
-    if (args.size() == 1 || args[1] == "ut") {
-        // Run unit tests
-        try {
-            RunUnittests();
-        } catch (const TException &e) {
-            e.Display();
-        }
-    } else if (args[1] == "wick-example") {
-        GenerateContractionsForSampleTask();
-    } else if (args[1] == "solve-1") {
-        IntegrationTest_Solve1();
-    } else if (args[1] == "solve-2") {
-        IntegrationTest_Solve2();
-    } else if (args[1] == "solve-3") {
-        IntegrationTest_Solve3();
-    } else if (args[1] == "pre-gui") {
-        PreGui();
-    } else {
-        if (args[1] != "help") {
-            MESSAGE("Unknown action: " << args[1]);
-            MESSAGE("");
-        }
-        
-        MESSAGE("Available actions:");
-        
-        MESSAGE("bin/femu ut (or just bin/femu) -- run unittests");
-        MESSAGE("bin/femu wick-example          -- run example wick task");
-        MESSAGE("bin/femu solve-1               -- run integration test `solve-1'");
-        MESSAGE("bin/femu solve-2               -- run integration test `solve-2'");
-        MESSAGE("bin/femu solve-3               -- run integration test `solve-3'");
-        MESSAGE("bin/femu pre-gui               -- run pre-gui solution");
-        MESSAGE("bin/femu help                  -- show this info");
-    }
+    UI_Step1 step_1;
+    step_1.show();
 
-    return 0;
+    return app.exec();
 }
