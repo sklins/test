4b94ffc12ebbd37d649d98241b9e358739f414e0
sklins <sonnik_007@mail.ru>
2014-05-09 20:04:10 +0000

    better singleton

--- femu/src/util/singleton.h
+++ femu/src/util/singleton.h
@@ -2,7 +2,26 @@
 #include "global.h"
 
 template <class T>
+class TSingleton {
+    static T* Handle(bool sw, T* val) {
+        static T* instance = new T();
+        if (sw) {
+            instance = val;
+            return NULL;
+        } else return instance;
+    }
+
+public:
+    static T* Instance() {
+        return Handle(false, NULL);
+    }
+
+    static void Switch(T* val) {
+        Handle(true, val);
+    }
+};
+
+template <class T>
 T* Singleton() {
-    static T* instance = new T();
-    return instance;
+    return TSingleton<T>::Instance();
 }
