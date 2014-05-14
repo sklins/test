c40b988301479f47c5d51b20a25d7140bf55522e
sklins <sonnik_007@mail.ru>
2014-05-14 09:28:41 +0000

    Stable

--- femu/src/ut/tests.cpp
+++ femu/src/ut/tests.cpp
@@ -1,8 +1,6 @@
 #include "tests.h"
 
 void RunUnittests() {
-    UT_Num();
-    UT_Complex();
     UT_WickSlot();
     MESSAGE("Unittests: OK");
 }
--- femu/src/ut/tests.h
+++ femu/src/ut/tests.h
@@ -2,8 +2,6 @@
 #include "ut_tools.h"
 
 // List of UT functions
-void UT_Num();
-void UT_Complex();
 void UT_WickSlot();
 
 // Run all UT functions
